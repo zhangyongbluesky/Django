dbda62debe92d5edf34a1e87f7a59984d33f2dd8
rockiezhang <zhangyongbluesky@gmail.com>
2016-08-05 23:29:13 +0800

    add templates

--- HelloWorld/HelloWorld/view.py
+++ HelloWorld/HelloWorld/view.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
+'''
 def hello(request):
 	return HttpResponse("Hello world ! ")
+'''
+
+def hello(request):
+    context          = {}
+    context['hello'] = 'Hello World!'
+    return render(request, 'hello.html', context)
